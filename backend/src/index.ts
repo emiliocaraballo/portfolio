e241e6a532002fa5b3de2c7c0a19c8ccb03ab37d
Emilio Caraballo <emiliocaraballo9810@gmail.com>
2022-01-19 12:25:44 -0500

    limpiar codigo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,5 @@
 require('dotenv').config();
 import app from './app'
-import { general } from './config/general';
 const main = async (): Promise<void> => {
     app.listen(app.get("port"));
     console.log("SERVER "+app.get("port"));
