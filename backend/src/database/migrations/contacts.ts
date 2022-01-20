f4f975aa0610a7ffdfd77ebc5fd7a1037c86f276
Emilio Caraballo <emiliocaraballo9810@gmail.com>
2022-01-19 23:40:48 -0500

    v1.2

--- backend/src/database/migrations/contacts.ts
+++ backend/src/database/migrations/contacts.ts
@@ -1,5 +1,9 @@
 import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
+    await knex.raw("SET TIMEZONE='America/Bogota'");
+    
+    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    
     await knex.schema.createTable('contacts', (table: Knex.TableBuilder) => {
         table.increments('sequence').primary();
         table.uuid('id').unique().defaultTo(knex.raw("uuid_generate_v4()"));
